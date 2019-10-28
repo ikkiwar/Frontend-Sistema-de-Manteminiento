91d375789383d1038b0f2d5dc8b3cbd8c0e15ce8
ikkiwar <you@example.com>
2019-10-28 16:20:52 -0600

    subiendo archivo con los componentes

---
+++ sistema_mantenimiento/src/components/side-bar.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { slide as Menu } from 'react-burger-menu';
+import Casa from '../images/casa.png';
+import Orden from '../images/orden.png';
+import Invent from '../images/editar.png'
+import Humano from '../images/usuario.png';
+import Ajustes from '../images/ajustes.png';
+import './styles/sidebar.css';
+
+class Sidebar extends React.Component{
+render(){
+
+    return(<Menu>
+        <a className="menu-item" href="/" >
+         <img src={Casa}/> &nbsp; Inicio 
+        </a>
+        <hr/>
+        <a className="menu-item" href="#">
+        <img src={Orden}/> Ordenes de Mantenimiento
+        </a>
+        <hr/>
+        <a className="menu-item" href="#">
+          <img src={Invent}/> &nbsp; Inventario
+        </a>
+        <hr/>
+        <a className="menu-item" href="#">
+          <img src={Humano}/> &nbsp; Recurso Humano
+        </a>
+        <hr/>
+        <a className="menu-item" href="#">
+          <img src={Ajustes}/> &nbsp; Ajustes
+        </a>
+        <hr/>
+        
+      </Menu>
+  )
+}
+
+
+}
+
+export default Sidebar
---
+++ sistema_mantenimiento/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Sidebar from './components/side-bar'
+
+
+const contenedor = document.getElementById("root")
+ReactDOM.render(
+<Sidebar/>, contenedor
+)
+
