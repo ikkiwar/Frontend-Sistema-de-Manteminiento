ebc399421f9e91f98b62904ae31429e50262ee51
Esperanza Duenas <da13002@ues.edu.sv>
2019-11-15 17:35:02 -0600

    Estilo para calendario

--- sistema_mantenimiento/src/components/Calendar.js
+++ sistema_mantenimiento/src/components/Calendar.js
@@ -1,17 +1,15 @@
 import React from "react";
 import Calendar from 'react-calendar'
-import Topbar from '../components/Topbar';
-import Titulo from '../components/Titulo';
-import Home from '../images/casa.png';
-import Orden from '../images/expediente.png';
+
 export default class CalendarComponent extends React.Component{
+    state ={
+        date: new Date(),
+    }
+
     render() {
         return (
             <div>
-                <Topbar />
-                <div><Titulo titulo="Inicio" icono={Home} />
-                </div>
-                <Calendar/>
+                <Calendar/> 
             </div>
         );
     }
