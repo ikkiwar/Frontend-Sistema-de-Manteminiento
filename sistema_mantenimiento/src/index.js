2164b296a5fc528c86d62894fc341f6cf024d8d5
jcrd1994 <31782411+jcrd1994@users.noreply.github.com>
2019-11-15 18:28:57 -0600

    detalles

--- sistema_mantenimiento/src/index.js
+++ sistema_mantenimiento/src/index.js
@@ -2,27 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Inicio from './pages/Inicio';
 import Inventario from './pages/Inventario';
+import Orden from './pages/orden';
 
 
 
 const contenedor = document.getElementById("root")
 
 
-ReactDOM.render(
-    <Inicio/> // pagina orden de trabajo
-    , contenedor
+//ReactDOM.render(
+  //  <Inicio/> // pagina orden de trabajo
+    //, contenedor
  
-    )
+    //)
+
+    
 ReactDOM.render(
     <Orden/> // se esta renderizando directamente la pagina orden
     , contenedor
 )
 
-ReactDOM.render(
-    <Inventario/> // se esta renderizando la pagina inventario
-    , contenedor
-)
 
+//ReactDOM.render(
+  //      <Inventario/> // se esta renderizando la pagina inventario
+    //    , contenedor
+    //)
 
 
   
