7edfa8cb22fe7bbbd11409181278ce95a15c7409
erickGuirola <gl13003@ues.edu.sv>
2019-11-15 18:13:05 -0600

    Mapeo en tabla Ordenes de Trabajo Fetch

--- sistema_mantenimiento/src/pages/Inicio.js
+++ sistema_mantenimiento/src/pages/Inicio.js
@@ -1,77 +1,85 @@
-import React from 'react';
-import Topbar from '../components/Topbar';
-import Table from '../components/Table';
-import Titulo from '../components/Titulo';
-import Home from '../images/casa.png';
-import Orden from '../images/expediente.png';
-class Inicio extends React.Component{
-    // esta es una pagina esa contiene todos los componentes que va contener la pagina de inicio
+import React from "react";
+import Topbar from "../components/Topbar";
+import Table from "../components/Table";
+import Titulo from "../components/Titulo";
+import Home from "../images/casa.png";
+import Orden from "../images/expediente.png";
+class Inicio extends React.Component {
+  // esta es una pagina esa contiene todos los componentes que va contener la pagina de inicio
 
-render(){
+  render() {
     const columnas = [
-        {
-            Header: "ID",
-            accessor: "idRecursoHumano",
-            style: {
-                textAlign: "center"
-            },
-            width: 100,
-            maxWidth: 100,
-            minWidth: 100
+      {
+        Header: "Id",
+        accessor: "ordenes",
+        style: {
+          textAlign: "center"
         },
-        {
-            Header: "Encargado",
-            accessor: "nombre",
-            style: {
-                textAlign: "center"
-            }
-        },
-        {
-            Header: "Estado",
-            accessor: "apellido",
-            style: {
-                textAlign: "center"
-            }
-           
-        },
-        {
-            Header: "Fecha de Generacion",
-            accessor: "telefono",
-            style: {
-                textAlign: "center"
-            }
-        },
-        {
-            Header: "Acciones",
-            Cell: props => {
-            
-                return (
-                    <button className="detalle">Detalle</button>
-    
-                )
-            },
-            width: 100,
-            maxWidth: 100,
-            minWidth: 100,
-            filterable: false,
-            sortable: false,
-            style: {
-                textAlign: "center"
-            }
+        width: 100,
+        maxWidth: 100,
+        minWidth: 100
+      },
+      {
+        Header: "Encargado",
+        accessor: "recursosHumanos.nombre",
+        style: {
+          textAlign: "center"
         }
-    
-    ]
+      },
+      {
+        Header: "Estado",
+        accessor: "estados.estado",
+        style: {
+          textAlign: "center"
+        }
+      },
+      {
+        Header: "Fecha de Generacion",
+        accessor: "ordenesfecha",
+        style: {
+          textAlign: "center"
+        }
+      },
+      {
+        Header: "Mantenimiento",
+        accessor: "mantenimientos.mantenimiento",
+        style: {
+          textAlign: "center"
+        }
+      },
+      {
+        Header: "Acciones",
+        Cell: props => {
+          return <button className="detalle">Detalle</button>;
+        },
+        width: 100,
+        maxWidth: 100,
+        minWidth: 100,
+        filterable: false,
+        sortable: false,
+        style: {
+          textAlign: "center"
+        }
+      }
+    ];
 
-    return(<div>
+    return (
+      <div>
         <Topbar />
-        <div><Titulo titulo="Inicio" icono={Home} />
+        <div>
+          <Titulo titulo="Inicio" icono={Home} />
         </div>
-        <Table entidad="/ordentrabajo" columnas = {columnas} title="Ordenes de Trabajo Activas"
-        icono={Orden} titulo="Nueva Orden" enlace="/OrdenTrabajo "
+        <Table
+          entidad="/ordentrabajo"
+          columnas={columnas}
+          title="Ordenes de Trabajo Activas"
+          icono={Orden}
+          titulo="Nueva Orden"
+          enlace="/OrdenTrabajo "
         />
-        
-    </div>)
-}
+      </div>
+    );
+  }
 }
 
-export default Inicio
+export default Inicio;
