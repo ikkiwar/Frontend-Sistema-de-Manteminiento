9832e33fdc7dcf1e400fc171f93bd0c142a338bf
erickGuirola <gl13003@ues.edu.sv>
2019-11-17 21:42:36 -0600

    Nueva Vista de Procedimientos por Equipos

--- sistema_mantenimiento/src/components/App.js
+++ sistema_mantenimiento/src/components/App.js
@@ -1,31 +1,29 @@
-import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Inicio from '../pages/Inicio';
-import Orden from '../pages/orden';
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Inicio from "../pages/Inicio";
+import Orden from "../pages/orden";
 import Inventario from "../pages/Inventario";
-import NotFound from '../components/NotFound';
-import Recurso from '../pages/RecursoHumano';
+import NotFound from "../components/NotFound";
+import Recurso from "../pages/RecursoHumano";
 import Calendario from "../pages/Calendario";
-import NuevoRecurso from '../pages/NuevoRecurso';
+import NuevoRecurso from "../pages/NuevoRecurso";
+import Procedimiento from "../pages/Procedimientos";
 
 function App() {
-
-    return (
-        // Este componente es el que contiene las rutas de los componentes y se encarga de renderiarlos
-        <BrowserRouter>
-            <Switch> 
-                <Route exact path="/Inicio" component={Inicio} />
-                <Route exact path="/OrdenTrabajo" component={Orden} />
-                <Route exact path="/RecursoHumano" component={Recurso}/>
-                <Route exact path="/Inventario" component={Inventario}/>
-                <Route exact path="/Calendario" component={Calendario}/>
-                <Route exact path="/NuevoRecurso" component={NuevoRecurso}/>
-                <Route component={NotFound} />
-            </Switch>
-
-        </BrowserRouter>
-
-    )
-
+  return (
+    // Este componente es el que contiene las rutas de los componentes y se encarga de renderiarlos
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/Inicio" component={Inicio} />
+        <Route exact path="/OrdenTrabajo" component={Orden} />
+        <Route exact path="/RecursoHumano" component={Recurso} />
+        <Route exact path="/Inventario" component={Inventario} />
+        <Route exact path="/Calendario" component={Calendario} />
+        <Route exact path="/NuevoRecurso" component={NuevoRecurso} />
+        <Route exact path="/Procedimiento" component={Procedimiento} />
+        <Route component={NotFound} />
+      </Switch>
+    </BrowserRouter>
+  );
 }
-export default App 
+export default App;
--- sistema_mantenimiento/src/components/Topbar.js
+++ sistema_mantenimiento/src/components/Topbar.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import Sidebar from './side-bar';
-import './styles/topbar.css';
-import Ues from '../images/ues.png';
-import { Link } from 'react-router-dom';
-
+import React from "react";
+import Sidebar from "./side-bar";
+import "./styles/topbar.css";
+import Ues from "../images/ues.png";
+import { Link } from "react-router-dom";
 
 class Topbar extends React.Component {
-
-    render() {
-        return (
-            <div className="top-bar">
-                <Sidebar />
-                <div className="top-bar-letter"> <img src={Ues} className="icono" />
-                    Sistema de Mantenimiento 
-                    </div>
-
-            </div>
-
-        )
-
-    }
-
+  render() {
+    return (
+      <div className="top-bar">
+        <Sidebar />
+        <div className="top-bar-letter">
+          {" "}
+          <img src={Ues} className="icono" />
+          Sistema de Mantenimiento/Administración de Centros de Computo
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Topbar
+export default Topbar;
