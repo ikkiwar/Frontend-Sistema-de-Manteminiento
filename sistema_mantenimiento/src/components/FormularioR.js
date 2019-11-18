956250bc440e79e61dfbee42a310bdc21c4bc73e
jcrd1994 <31782411+jcrd1994@users.noreply.github.com>
2019-11-17 23:31:36 -0600

    alineacion de divs

--- sistema_mantenimiento/src/components/FormularioR.js
+++ sistema_mantenimiento/src/components/FormularioR.js
@@ -34,22 +34,18 @@ class FormularioR extends React.Component {
         return (
             <div>
 
-                <div style={{
-                    position: "relative", overflow: "hidden", marginLeft: "20%", marginRight: "20%", border: "solid #0a0a0a 5px",
-
-                    boxShadow: "8px 8px 10px 0px #818181", marginTop: "0%", borderRadius: "10px 10px 10px 10px", marginBottom: "30px"
-                }}>
+                <div >
 
                     <h1 style={{ textAlign: "center" }}>NUEVO RECURSO HUMANO</h1>
 
-                    <div style={{ padding: "10px", marginLeft: "40px", margintop: "20px", float: "left" }}>
+                    <div style={{ padding: "10px", marginLeft: "80px", margintop: "20px" }}>
                         <h3>Nombre:</h3>
                         <span className="p-float-label">
                             <InputText id="float-input" type="text" size="40" value={this.state.value1} onChange={(e) => this.setState({ value1: e.target.value })} />
-                            <label htmlFor="float-input">nombre</label>
+                            <label htmlFor="float-input">Nombre</label>
                         </span></div>
 
-                    <div style={{ padding: "10px", marginLeft: "40px", margintop: "20px", float: "left" }}>
+                    <div style={{ padding: "10px", marginLeft: "80px", margintop: "20px" }}>
                         <h3>Apellido:</h3>
                         <span className="p-float-label">
                             <InputText id="float-input" type="text" size="50" value={this.state.value2} onChange={(e) => this.setState({ value2: e.target.value })} />
@@ -57,7 +53,7 @@ class FormularioR extends React.Component {
                         </span></div>
 
 
-                    <div style={{ padding: "10px", marginLeft: "50px", margintop: "20px", float: "left" }}>
+                    <div style={{ padding: "10px", marginLeft: "80px", margintop: "20px" }}>
                         <h3>Número Telefonico</h3>
                         <span className="p-float-label">
                             <InputText id="float-input" type="text" keyfilter="pint" value={this.state.value3} onChange={(e) => this.setState({ value3: e.target.value })} />
@@ -65,7 +61,7 @@ class FormularioR extends React.Component {
                         </span>
                     </div>
 
-                    <div style={{ padding: "10px", marginLeft: "50px", margintop: "50px", float: "left", marginRight: "50px" }}>
+                    <div style={{ padding: "10px", marginLeft: "80px", margintop: "50px"}}>
                         <h3>Cargo</h3>
                         <span className="p-float-label">
                             <InputText id="float-input" type="text" size="30" value={this.state.value4} onChange={(e) => this.setState({ value4: e.target.value })} />
@@ -74,7 +70,7 @@ class FormularioR extends React.Component {
                     </div>
 
 
-                    <div style={{ float: "right", padding: "10px", marginLeft: "30px", margintop: "30px", textAlign: "end" }}>
+                    <div style={{ float: "right", padding: "10px", margintop: "30px", textAlign: "end", marginRight: "40px"}}>
                         <Button label="Guardar" icon="pi pi-check" className="p-button-success" />
                         <Button label="Cancelar" icon="pi pi-times" className="p-button-danger" />
 
--- sistema_mantenimiento/src/pages/NuevoRecurso.js
+++ sistema_mantenimiento/src/pages/NuevoRecurso.js
@@ -7,8 +7,14 @@ class NuevoRecurso extends React.Component {
     render() {
         return (
             <div>
-                <Topbar />
+                <Topbar /><br></br>
+
+            <div id="contenedor" style={{overflow: "hidden", marginLeft: "20%", marginRight: "20%", border: "solid #0a0a0a 5px",
+             boxShadow: "8px 8px 10px 0px #818181", marginTop: "5%", borderRadius: "10px 10px 10px 10px", marginBottom:"30px"}}>    
                 <FormularioR />
+                </div> 
+            
+            
             </div>
 
 
